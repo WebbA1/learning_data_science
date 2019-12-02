0d3a2959ea7481b40bf1e6565afc4974936c5461
Aaron Webb <Aaron@Aarons-MacBook-Air.local>
2019-12-02 18:56:00 +0000

    Commit from RStudio on Aarons Mac

--- Data Science/Tree Based Models/Scripts/trees_with_iris.R
+++ Data Science/Tree Based Models/Scripts/trees_with_iris.R
@@ -2,29 +2,60 @@ library(tidyverse)
 library(caret)
 library(rpart)
 library(rpart.plot)
+library(ranger)
+library(xgboost)
+
+
+# Keep 25% of the data for tetsing
+set.seed(64736)
+partition_index <- caret::createDataPartition(iris$Species, 
+                                              times = 1, 
+                                              p = 0.75,
+                                              list = FALSE)
+iris_train <- iris[partition_index, ]
+iris_test <- iris[-partition_index, ]
 
 # Train a tree model with the Iris data
 ctrl_boot <- trainControl(method = "boot",
                           number = 999)
 ctrl_cv <- trainControl(method = "cv", 
-                        number = 20)
+                        number = 5)
 
 iris_rpart_model <- train(Species ~ ., 
-                          data = iris, 
+                          data = iris_train, 
                           method = "rpart",
                           trControl = ctrl_cv, 
                           tuneLength = 20)
-iris_rpart_model
-iris_rpart_model$results
-final_model <- rpart(Species ~ ., 
-                     data = iris,
-                     cp = iris_rpart_model$bestTune[[1]])
-rpart.plot(final_model)
+predicted_values_rpart <- predict(iris_rpart_model, 
+                                  newdata = iris_test)
+test_set_rpart <- data.frame(obs = iris_test$Species,
+                             pred = predicted_values_rpart)
+confusionMatrix(data = test_set_rpart$pred, 
+                reference = test_set_rpart$obs)
 
 # Train a random forest with the Iris data
-library(ranger)
 iris_train_random_forest <- train(Species ~ .,
-                                  data = iris,
+                                  data = iris_train,
                                   method = "ranger",
                                   trControl = ctrl_cv,
-                                  tunelength = 20)
+                                  tuneLength = 20)
+
+predicted_values_random_forest <- predict(iris_train_random_forest, 
+                                          newdata = iris_test)
+test_set_rf <- data.frame(obs = iris_test$Species, 
+                          pred = predicted_values_random_forest)
+confusionMatrix(data = test_set_rf$pred, 
+                reference = test_set_rf$obs)
+
+# Train an XG Boost model
+iris_train_xgboost <- train(Species ~ .,
+                            data = iris_train,
+                            method = "xgbLinear",
+                            trControl = ctrl_cv,
+                            tuneLength = 5)
+predicted_values_xgboost<- predict(iris_train_xgboost, 
+                                          newdata = iris_test)
+test_set_xgboost <- tibble(obs = iris_test$Species,
+                           pred = predicted_values_xgboost)
+confusionMatrix(data = test_set_xgboost$pred,
+                reference = test_set_xgboost$obs)
