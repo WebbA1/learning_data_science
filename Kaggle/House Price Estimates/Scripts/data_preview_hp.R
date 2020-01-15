cd39330f2dd39f4ae9e751d30ddb7d98126a4eaf
Aaron Webb <aaron.webb@mediacom.com>
2020-01-15 17:45:27 +0000

    Commit from RStudio on Aarons Work Laptop

--- Kaggle/House Price Estimates/Scripts/data_preview_hp.R
+++ Kaggle/House Price Estimates/Scripts/data_preview_hp.R
@@ -2,11 +2,31 @@
 # Library
 library(tidyverse)
 library(caret)
-library(anom)
+library(anomalize)
+library(corrplot)
 
+# Load in mapping for types
+types_mapping <- read_csv("./Kaggle/House Price Estimates/Inputs/identify_column_class_complete.csv") %>%
+  pull(type) %>%
+  paste(collapse = "")
 # Load in the training dataset (Class = SalePrice)
-hp_training_data <- read_csv("./Kaggle/House Price Estimates/Data/train.csv")
+# TODO Define the class for each column
+hp_data <- read_csv("./Kaggle/House Price Estimates/Data/train.csv", col_types = types_mapping)
 str(hp_training_data)
+
+# Converting to numeric values for the regression
+dmy <- dummyVars(" ~ .", 
+                 data = hp_data)
+hp_data_transformed <- data.frame(predict(dmy, 
+                                          newdata = hp_data)) %>%
+  as_tibble()
+
+# Split the data into a training and validation dataset
+set.seed(524)
+random_rows <- floor(sample(1:nrow(hp_data_transformed), size = nrow(hp_data_transformed) * 0.8))
+hp_training_data <- hp_data_transformed[random_rows, ]
+hp_validation_data <- hp_data_transformed[-random_rows, ]
+
 
 # Class Deep Dive ----
 # Closer look at the class variable
@@ -26,3 +46,50 @@ hp_training_data %>%
 
 # Variable Deep Dive ----
 # Closer look at the correlation and interation of the variables and the class
+cor()
+# PCA on the variables
+# Closer look at how the numeric values affect the SalePrice
+continous_numeric_values <- hp_training_data %>%
+  select(SalePrice, LotFrontage, LotArea, MasVnrArea, BsmtFinSF1, BsmtFinSF2,
+         BsmtUnfSF, TotalBsmtSF, `1stFlrSF`, `2ndFlrSF`, LowQualFinSF, 
+         GrLivArea, WoodDeckSF, OpenPorchSF, EnclosedPorch, `3SsnPorch`, 
+         ScreenPorch, PoolArea, MiscVal, GarageArea) %>%
+  gather("key", "value", -SalePrice) 
+# Lot Area
+continous_numeric_values %>%
+  filter(value != 0,
+         key == "LotArea") %>%
+  ggplot(aes(x = value, y = SalePrice, colour = key)) +
+  geom_point() +
+  facet_wrap(~key) +
+  geom_smooth(method = "auto", aes(colour = "blue")) +
+  theme_dark()
+
+# Lot Area
+continous_numeric_values %>%
+  filter(value != 0,
+         key %in% c("TotalBsmtSF", "1stFlrSF"),
+         !is.na(value)) %>%
+  ggplot(aes(x = value, y = SalePrice, colour = key)) +
+  geom_point() +
+  facet_wrap(~key) +
+  geom_smooth(method = "lm", formula = "y ~ x") +
+  theme_dark()
+
+discrete_numeric_values <- hp_training_data %>%
+  select(SalePrice, BsmtFullBath, BsmtHalfBath, FullBath, HalfBath, BedroomAbvGr,
+         KitchenAbvGr, TotRmsAbvGrd, Fireplaces, GarageCars) %>%
+  gather("key", "value", -SalePrice)
+
+# Bedrooms above ground and total rooms above ground
+discrete_numeric_values %>%
+  filter(key %in% c("BedroomAbvGr", "TotRmsAbvGrd")) %>%
+  ggplot(aes(x = value, y = SalePrice, colour = key, group = value)) +
+  geom_boxplot() +
+  facet_wrap(~key)
+# 
+discrete_numeric_values %>%
+  filter(!key %in% c("BedroomAbvGr", "TotRmsAbvGrd")) %>%
+  ggplot(aes(x = value, y = SalePrice, colour = key, group = value)) +
+  geom_boxplot() +
+  facet_wrap(~key)
